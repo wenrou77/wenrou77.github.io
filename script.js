22f51c2a20c13a1446578489e64b4887360027ac
wenrou77 <64522463+wenrou77@users.noreply.github.com>
2020-08-17 21:41:24 +0800

    add title and code format

--- script.js
+++ script.js
@@ -2,34 +2,34 @@ var button = document.getElementById("create");
 var input = document.getElementById("input");
 var list = document.getElementById("list");
 
-function delData(index){
+function delData(index) {
   // 刪除資料
-  todolist.splice(index,1);
+  todolist.splice(index, 1);
   //更新顯示畫面
   render()
 }
 
 //印出來矩陣
-function render(){
+function render() {
   localStorage.setItem("listItems", JSON.stringify(todolist))
-  var content="";
-  for(i=0; i<=todolist.length-1; i++){
-    content=content+"<div><button class='buttonstyle' onclick='delData("+i+")'>"+"刪除"+"</button>"+todolist[i]+"</div>";
+  var content = "";
+  for (i = 0; i <= todolist.length - 1; i++) {
+    content = content + "<div><button class='buttonstyle' onclick='delData(" + i + ")'>" + "刪除" + "</button>" + todolist[i] + "</div>";
   }
-  list.innerHTML=content
+  list.innerHTML = content
 }
 
-var todolist= JSON.parse(localStorage.getItem("listItems")) || [];
+var todolist = JSON.parse(localStorage.getItem("listItems")) || [];
 render()
 
-function addData(){
-  if(input.value != "" && input.value !=" "){
+function addData() {
+  if (input.value != "" && input.value != " ") {
     todolist.push(input.value);
-    input.value="";
+    input.value = "";
     render()
-  }else {
+  } else {
     alert("you didn't input anything, Please enter again")
   }
 
-//  alert(input.value)
+  //  alert(input.value)
 }
